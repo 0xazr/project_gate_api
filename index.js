5b08371d783696748daa7a39bf2a0a08f1edb223
Yuniarrr <5027211025@mhs.its.ac.id>
2023-03-30 09:25:42 +0700

    fix code

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ app.get('/', MIDDLEWARE.validateParams, (req, res) => {
     }
 });
 
-app.put('/masuk/:gate_id/:card_id', MIDDLEWARE.checkParams, (req, res) => {
+app.get('/masuk/:gate_id/:card_id', MIDDLEWARE.checkParams, (req, res) => {
     try {
         let connection = new Connection(config);
         const gateId = req.params.gate_id;
@@ -156,7 +156,7 @@ app.put('/masuk/:gate_id/:card_id', MIDDLEWARE.checkParams, (req, res) => {
     }
 });
 
-app.put('/keluar/:gate_id/:card_id', MIDDLEWARE.checkParams, (req, res) => {
+app.get('/keluar/:gate_id/:card_id', MIDDLEWARE.checkParams, (req, res) => {
     try {
         let connection = new Connection(config);
         const gateId = req.params.gate_id;
@@ -170,33 +170,64 @@ app.put('/keluar/:gate_id/:card_id', MIDDLEWARE.checkParams, (req, res) => {
                 });
             }
 
-            let query = APP.exitQuery(gateId, cardId);
+            let query = APP.getCard(cardId);
+            query += APP.getGate(gateId);
+            console.log(query)
 
-            let request = new Request(
-                Util.format(query),
-                function (err, rowCount, rows) {
-                    if (err) {
-                        console.log(err);
-                        return res.status(500).send({
-                            message: 'Error updating data into database'
-                        });
-                    }
-                    console.log(rowCount + ' row(s) inserted');
-                    connection.close();
+            let data = [];
+            let request = new Request(query, function (err, rowCount, rows) {
+                if (err) {
+                    console.log(err);
+                    return res.status(500).send({
+                        message: 'Error getting data from database'
+                    });
+                }
+                console.log(rowCount + ' row(s) returned');
+
+                if (rowCount == 1 || rowCount == 0) {
+                    return res.status(404).send({
+                        message: `id_kartu_akses / id_tipe_gate is not valid`
+                    });
+                }
+
+                let is_aktif = APP.checkData(data);
+
+                return res.status(200).send({
+                    message: `Keluar gate ${gateId} dengan kartu akses ${cardId}`,
+                    is_aktif: is_aktif
                 });
+            });
 
-            request.on('doneProc', function () {
-                console.log("Querying is done...");
-            })
+            request.on('row', function (columns) {
+                let item = {};
+                columns.forEach(function (column) {
+                    if (column.value === null) {
+                        item[column.metadata.colName] = '';
+                    } else {
+                        item[column.metadata.colName] = column.value;
+                    }
+                });
+                data.push(item);
+            });
+
+            request.on('requestCompleted', function () {
+                connection.close();
+            });
+
+            request.on('error', function (err) {
+                console.log(err);
+                connection.close();
+                return res.status(500).send({
+                    message: 'Internal server error'
+                });
+            });
 
             connection.execSql(request);
-            return res.status(200).send({
-                message: `Keluar gate in ${gateId} & ${cardId}`
-            });
         });
 
         connection.connect();
     } catch (error) {
+        console.log(error);
         return res.status(500).send({
             message: 'Internal server error'
         });
